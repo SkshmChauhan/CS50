a50cd7448e219747f5a2d558e6ce0f361247a91a
Saksham Chauhan <adidrcool@gmail.com>
2021-06-16 01:29:10 +0530

    Renamed files

---
+++ CS50's_Introduction_to_CS/Week1/addition.c
@@ -0,0 +1,14 @@
+//
+// Created by saksham on 16-06-2021.
+//
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+    printf("%i\n", x+y);
+}
+
