9d3e709ee9f8fcc8761ca9ad25c8d97c3774014c
Saksham Chauhan <adidrcool@gmail.com>
2021-07-09 16:02:52 +0530

    Added Week 6

---
+++ CS50's_Introduction_to_CS/Week6/hello.py
@@ -0,0 +1,8 @@
+from cs50 import get_string
+
+# print("Hello, World")
+
+name = get_string("What's your name? ")
+print("Hello, " + name)
+# Below is illustration of formatted strings
+print(f"Hello, {name}")
