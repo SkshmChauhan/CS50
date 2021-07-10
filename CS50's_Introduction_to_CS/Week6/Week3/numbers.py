09a445e5c7ea02dd478cf19c2d3c9a8766fc1338
Saksham Chauhan <adidrcool@gmail.com>
2021-07-10 18:16:22 +0530

    Rewrote Week 3 in Python

---
+++ CS50's_Introduction_to_CS/Week6/Week3/numbers.py
@@ -0,0 +1,9 @@
+import sys
+
+numbers = [4, 6, 8, 2, 7, 5, 0]
+
+key = 0
+if key in numbers:
+    print("Found")
+else:
+    print("Not Found")
---
+++ CS50's_Introduction_to_CS/Week6/Week3/phonebook.py
@@ -0,0 +1,14 @@
+# Implements a phone book
+# from cs50 import get_string
+
+people = {
+    "Brian": "+1-617-495-1000",
+    "David": "+1-949-468-2750"
+}
+
+# Search for name
+name = input("Name:   ")
+if name in people:
+    print(f"Number: {people[name]}")
+else:
+    print("Number doesn't exists.")
